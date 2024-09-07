cd6ae2d0ef7144147d6a18c03b434e886cdb62a3
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-07 05:27:34 +0300

    api_routes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Logo from "@/components/Logo";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
